52055c22e176df2dff2dfdffcc63aac5ddebf71c
German Odilov <godilov@skiff.com>
2023-12-23 18:53:42 +0600

    feat(init): init flags, hypr and refactoring

--- init.sh
+++ init.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-git clone git@github.com:godilov/nd-dotfiles.git dotfiles
+DIR=$(pwd)
+PATTERN="^[a-zA-Z0-9_-]+$"
 
-cd dotfiles
+function install{
+    paru -S --needed $($1 | grep -E --color=never $PATTERN)
+}
 
-rm -rf ext/nd/*
+function install-file {
+    paru -S --needed $(cat $1 | grep -E --color=never $PATTERN)
+}
+
+if [[ "$*" == *"-c"* || "$*" == *"--clone"* ]]
+then
+    git clone git@github.com:godilov/nd-dotfiles.git dotfiles
+
+    cd dotfiles
+fi
+
+rm -rf ext/*
+
+if [[ "$*" == *"--ext"* ]]
+then
+    git clone git@github.com:Morganamilo/paru.git ext/paru
+    git clone git@github.com:bobafetthotmail/refind-theme-regular.git ext/refind
+fi
 
 git clone git@github.com:godilov/nd-dotfiles-lib.git ext/nd/lib
 git clone git@github.com:godilov/nd-dotfiles-res.git ext/nd/res
@@ -25,3 +45,74 @@ ln -s ../../../lib ext/nd/awesome/ext/nd/lib
 
 ln -s ../../../res ext/nd/nvim/ext/nd/res
 ln -s ../../../res ext/nd/awesome/ext/nd/res
+
+if [[ "$*" == *"--all"* ]]
+then
+    install $(cat pkg/dev pkg/shell pkg/fonts pkg/apps)
+fi
+
+if [[ "$*" == *"--dev"* ]]
+then
+    install-file pkg/dev
+fi
+
+if [[ "$*" == *"--shell"* ]]
+then
+    install-file pkg/shell
+fi
+
+if [[ "$*" == *"--fonts"* ]]
+then
+    install-file pkg/fonts
+fi
+
+if [[ "$*" == *"--apps"* ]]
+then
+    install-file pkg/apps
+fi
+
+if [[ "$*" == *"--amd"* ]]
+then
+    install-file pkg/v_amd
+fi
+
+if [[ "$*" == *"--nvidia"* ]]
+then
+    install-file pkg/v_nvidia
+fi
+
+if [[ "$*" == *"--nvim"* ]]
+then
+    rm -rf ~/.config/nvim
+
+    ln -sf $DIR/ext/nd/nvim ~/.config/nvim
+fi
+
+if [[ "$*" == *"--alacritty"* ]]
+then
+    rm -rf ~/.config/alacritty
+
+    ln -sf $DIR/config/alacritty ~/.config/alacritty
+fi
+
+if [[ "$*" == *"--hypr"* ]]
+then
+    rm -rf ~/.config/hypr
+    rm -rf ~/.config/waybar
+
+    install-file pkg/wm_hyprland
+
+    ln -sf $DIR/config/hypr ~/.config/hypr
+    ln -sf $DIR/config/waybar ~/.config/waybar
+fi
+
+if [[ "$*" == *"--awesome"* ]]
+then
+    rm -rf ~/.config/awesome
+
+    install-file pkg/wm_awesome
+
+    ln -sf $DIR/ext/nd/awesome ~/.config/awesome
+fi
+
+cd -
