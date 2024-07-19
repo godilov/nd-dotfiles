0e91b75469bd27ae143307f70fb0b6557d5bf659
German Odilov <godilov@skiff.com>
2024-07-19 20:25:15 +0100

    refactor(res): better cache

--- ext/res/src/nd/res/nvim/cache.lua
+++ ext/res/src/nd/res/nvim/cache.lua
@@ -1,24 +1,81 @@
-local type_lib           = require 'nd.lib.type'
-local assert_lib         = require 'nd.lib.assert'
+local str_lib    = require 'nd.lib.str'
+local type_lib   = require 'nd.lib.type'
+local cache_lib  = require 'nd.lib.cache'
+local assert_lib = require 'nd.lib.assert'
 
-local cache              = require 'nd.res.cache'
-local colors_fn          = require 'nd.res.nvim.colors'
-local keys_fn            = require 'nd.res.nvim.keys'
+local colors_fn  = require 'nd.res.nvim.colors'
+local keys_fn    = require 'nd.res.nvim.keys'
 
-local is_str             = type_lib.is_str
-local is_tab             = type_lib.is_tab
+local concat2s   = str_lib.concat2s
+local concat3s   = str_lib.concat3s
 
-local nd_assert          = assert_lib.get_fn(ND_RES_IS_DEBUG)
+local is_str     = type_lib.is_str
+local is_tab     = type_lib.is_tab
 
-local get_colors_keyname = cache.get_colors_keyname
-local get_colors_scheme  = cache.get_colors_scheme
-local get_keys_keyname   = cache.get_keys_keyname
-local get_keys_scheme    = cache.get_keys_scheme
+local set        = cache_lib.fs.set
+local get        = cache_lib.fs.get
+
+local concat     = table.concat
+
+local nd_assert  = assert_lib.get_fn(ND_RES_IS_DEBUG)
 
 
-local get_colors = nil
-local get_keys = nil
+local get_colors_keyname    = nil
+local get_colors_scheme_raw = nil
+local get_colors_scheme     = nil
+local get_keys_keyname      = nil
+local get_keys_scheme_raw   = nil
+local get_keys_scheme       = nil
 
+local get_colors            = nil
+local get_keys              = nil
+
+
+get_colors_keyname = function(strs)
+    return concat2s(concat(strs, '-'), '.lua')
+end
+
+get_keys_keyname = function(type, scheme)
+    return concat3s(type, '-', scheme)
+end
+
+get_colors_scheme_raw = function(fn, config, key)
+    local scheme = fn(config)
+
+    set(key, scheme)
+
+    return scheme
+end
+
+get_keys_scheme_raw = function(fn, config, key)
+    local scheme = fn(config)
+
+    set(key, scheme)
+
+    return scheme
+end
+
+get_colors_scheme = function(fn, config, key, is_forced)
+    if is_forced then
+        return get_colors_scheme_raw(fn, config, key)
+    else
+        local scheme_cache = get(key)
+
+        return is_tab(scheme_cache) and scheme_cache or
+            get_colors_scheme_raw(fn, config, key)
+    end
+end
+
+get_keys_scheme = function(fn, config, key, is_forced)
+    if is_forced then
+        return get_keys_scheme_raw(fn, config, key)
+    else
+        local scheme_cache = get(key)
+
+        return is_tab(scheme_cache) and scheme_cache or
+            get_keys_scheme_raw(fn, config, key)
+    end
+end
 
 get_colors = function(config, is_forced)
     nd_assert(is_tab(config), 'config must be of type table')
