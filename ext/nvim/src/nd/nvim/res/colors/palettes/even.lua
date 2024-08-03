adbeb6b01bd4a5d48ebff1cf610ce4b38235fbcb
German Odilov <godilov@skiff.com>
2024-08-03 12:59:54 +0100

    feat(palette): hsl color scheme

--- ext/nvim/src/nd/nvim/res/colors/palettes/even.lua
+++ ext/nvim/src/nd/nvim/res/colors/palettes/even.lua
@@ -1,69 +1,69 @@
 local color_lib = require 'nd.lib.color'
 
-local hsb = color_lib.hsb.from
-local hex = color_lib.hsb.as_hex
-
-local hsb_val = {
-    { 0.7, 0.9 },
-    { 0.6, 1.0 },
-    { 0.4, 1.0 },
+local val = {
+    { 0.5, 0.5 },
+    { 1.0, 0.7 },
+    { 1.0, 0.8 },
 }
 
-local fn = nil
+local hsl = nil
 
 
-fn = function(val)
-    return hex(hsb(val))
+hsl = function(x)
+    local hsl = color_lib.hsl.from
+    local hex = color_lib.hsl.as_hex
+
+    return hex(hsl(x))
 end
 
 
 return function(_)
     return {
         fg = {
-            normal = fn { 0, 0, 1.00, 0 },
+            normal = hsl { 0, 0, 1.00, 0 },
         },
         bg = {
-            normal = fn { 0, 0, 0.00, 0 },
+            normal = hsl { 0, 0, 0.00, 0 },
         },
         black = {
-            dark   = fn { 0, 0, 0.05, 0 },
-            normal = fn { 0, 0, 0.10, 0 },
-            light  = fn { 0, 0, 0.15, 0 },
+            dark   = hsl { 0, 0, 0.05, 0 },
+            normal = hsl { 0, 0, 0.10, 0 },
+            light  = hsl { 0, 0, 0.15, 0 },
         },
         red = {
-            dark   = fn { 0, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 0, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 0, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 0, val[1][1], val[1][2], 0 },
+            normal = hsl { 0, val[2][1], val[2][2], 0 },
+            light  = hsl { 0, val[3][1], val[3][2], 0 },
         },
         yellow = {
-            dark   = fn { 30, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 30, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 30, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 30, val[1][1], val[1][2], 0 },
+            normal = hsl { 30, val[2][1], val[2][2], 0 },
+            light  = hsl { 30, val[3][1], val[3][2], 0 },
         },
         green = {
-            dark   = fn { 60, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 60, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 60, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 60, val[1][1], val[1][2], 0 },
+            normal = hsl { 60, val[2][1], val[2][2], 0 },
+            light  = hsl { 60, val[3][1], val[3][2], 0 },
         },
         blue = {
-            dark   = fn { 210, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 210, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 210, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 210, val[1][1], val[1][2], 0 },
+            normal = hsl { 210, val[2][1], val[2][2], 0 },
+            light  = hsl { 210, val[3][1], val[3][2], 0 },
         },
         magenta = {
-            dark   = fn { 330, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 330, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 330, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 330, val[1][1], val[1][2], 0 },
+            normal = hsl { 330, val[2][1], val[2][2], 0 },
+            light  = hsl { 330, val[3][1], val[3][2], 0 },
         },
         cyan = {
-            dark   = fn { 180, hsb_val[1][1], hsb_val[1][2], 0 },
-            normal = fn { 180, hsb_val[2][1], hsb_val[2][2], 0 },
-            light  = fn { 180, hsb_val[3][1], hsb_val[3][2], 0 },
+            dark   = hsl { 180, val[1][1], val[1][2], 0 },
+            normal = hsl { 180, val[2][1], val[2][2], 0 },
+            light  = hsl { 180, val[3][1], val[3][2], 0 },
         },
         white = {
-            dark   = fn { 0, 0, 0.85, 0 },
-            normal = fn { 0, 0, 0.90, 0 },
-            light  = fn { 0, 0, 0.95, 0 },
+            dark   = hsl { 0, 0, 0.85, 0 },
+            normal = hsl { 0, 0, 0.90, 0 },
+            light  = hsl { 0, 0, 0.95, 0 },
         },
     }
 end
