a24e95c853ed03634d0a098e8adba4d7a6f4dfa6
German Odilov <godilov@tuta.io>
2024-10-09 20:26:34 +0500

    fix(lsp): remark and marksman configs

--- config/nvim/src/nd/nvim/res/init.lua
+++ config/nvim/src/nd/nvim/res/init.lua
@@ -213,8 +213,8 @@ lsp_fn = function(config)
         { 'yamlls',        yaml_fn(config.yaml) },
         { 'taplo',         toml_fn(config.toml) },
 
-        { 'marksman',      marksman_fn(config.latex) },
-        { 'remark_ls',     remark_fn(config.latex) },
+        { 'marksman',      marksman_fn(config.marksman) },
+        { 'remark_ls',     remark_fn(config.remark) },
         { 'texlab',        texlab_fn(config.latex) },
     }
 end
