cac938d80f62927b2870a9128e40cebd425805ed
German Odilov <godilov@tuta.io>
2024-10-16 14:47:37 +0500

    feat(nvim): lualine fullpath

--- config/nvim/src/nd/nvim/appearance.lua
+++ config/nvim/src/nd/nvim/appearance.lua
@@ -30,6 +30,18 @@ return function(config)
             component_separators = { left = '|', right = '|' },
             section_separators = { left = '', right = '' },
         },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch', 'diff', 'diagnostics' },
+            lualine_c = { {
+                'filename',
+                file_status = true,
+                path = 1,
+            } },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
     }
 
     bufferline.setup {
