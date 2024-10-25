1b3adff28ff319d0af57aab86117392afa868353
German Odilov <godilov@tuta.io>
2024-10-25 20:29:04 +0500

    feat(pass): hyprland tofi password

---
+++ config/hypr/scripts/pass.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+STORE=~/.password-store
+STORE_PREF='password-store'
+
+FILES=$(fd '^[a-zA-Z0-9]*\.gpg$' $STORE | sed -E "s/^.*$STORE_PREF\/([a-zA-Z0-9/]+)\.gpg/\1/g")
+FILE=$(tofi <<< $FILES)
+
+if [[ -n $FILE ]]; then
+    pass show -c $FILE
+
+    notify-send "Password $FILE is copied"
+fi
