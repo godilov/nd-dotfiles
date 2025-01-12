832aafd7b09b4a22af6b2e58c05b04f0b7c265b9
German Odilov <godilov@tuta.io>
2025-01-12 10:47:53 +0500

    fix(nvim): bufferline offset

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -221,6 +221,14 @@ require 'lazy'.setup {
                 options = {
                     diagnostics = 'nvim_lsp',
                     always_show_bufferline = false,
+                    offsets = {
+                        {
+                            filetype = 'neo-tree',
+                            text = 'Neo-tree',
+                            highlight = 'Directory',
+                            text_align = 'left',
+                        },
+                    },
                 },
             },
             keys = {
